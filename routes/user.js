c48e7b7cdc1e0e18807f797d665b75bbc006b361
KarthikSharma-7 <karthiksharma72003@gmail.com>
2022-07-28 10:36:48 +0530

    added

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const router = express();
 const users = require("../controllers/user.js");
-
+const { authMiddleware } = require("../middleware/auth");
 
 //create new user
 // router.post("/create_user",users.create);
 
 //get user by id
-router.get("/get_user/:userid",users.findOne);
+router.get("/get_user/:userid", users.findOne);
 
-//get all users 
-router.get("/get_all_users",users.findAll);
+//get all users
+router.get("/get_all_users", users.findAll);
 
-
-//update user by userid 
-router.put("/update_user/:userid",users.update);
+//update user by userid
+router.put("/update_user/:userid", authMiddleware, users.update);
 
 //delete user by userid
-router.delete("/delete_user/:userid",users.delete);
-
+router.delete("/delete_user/:userid", authMiddleware, users.delete);
 
 module.exports = router;
